dcc817d001cb05c22400a6922e47da6747b3f69b
pbloods <lxtmltop@163.com>
2022-05-14 00:25:27 +0800

    typecho

--- config.inc.php
+++ config.inc.php
@@ -1,47 +1,31 @@
 <?php
-/**
- * Typecho Blog Platform
- *
- * @copyright  Copyright (c) 2008 Typecho team (http://www.typecho.org)
- * @license    GNU General Public License 2.0
- * @version    $Id$
- */
-
-/** 开启https */
-define('__TYPECHO_SECURE__',true);
-
-/** 定义根目录 */
+// site root path
 define('__TYPECHO_ROOT_DIR__', dirname(__FILE__));
 
-/** 定义插件目录(相对路径) */
+// plugin directory (relative path)
 define('__TYPECHO_PLUGIN_DIR__', '/usr/plugins');
 
-/** 定义模板目录(相对路径) */
+// theme directory (relative path)
 define('__TYPECHO_THEME_DIR__', '/usr/themes');
 
-/** 后台路径(相对路径) */
+// admin directory (relative path)
 define('__TYPECHO_ADMIN_DIR__', '/admin/');
 
-/** 设置包含路径 */
-@set_include_path(get_include_path() . PATH_SEPARATOR .
-__TYPECHO_ROOT_DIR__ . '/var' . PATH_SEPARATOR .
-__TYPECHO_ROOT_DIR__ . __TYPECHO_PLUGIN_DIR__);
+// register autoload
+require_once __TYPECHO_ROOT_DIR__ . '/var/Typecho/Common.php';
 
-/** 载入API支持 */
-require_once 'Typecho/Common.php';
+// init
+\Typecho\Common::init();
 
-/** 程序初始化 */
-Typecho_Common::init();
-
-/** 定义数据库参数 */
-$db = new Typecho_Db('Pdo_Mysql', 'typecho_');
+// config db
+$db = new \Typecho\Db('Pdo_Mysql', 'typecho_');
 $db->addServer(array (
   'host' => '173.82.226.222',
+  'port' => 3306,
   'user' => 'Pblood',
   'password' => 'fkdzsnxfe2xTfJzS',
   'charset' => 'utf8mb4',
-  'port' => '888',
   'database' => 'pblood',
-  'engine' => 'MyISAM',
-), Typecho_Db::READ | Typecho_Db::WRITE);
-Typecho_Db::set($db);
+  'engine' => 'InnoDB',
+), \Typecho\Db::READ | \Typecho\Db::WRITE);
+\Typecho\Db::set($db);
--- install.php
+++ install.php
@@ -770,7 +770,7 @@ function install_step_1_perform()
         }
     }
 
-    if ($writeable) {
+    if (!$writeable) {
         $errors[] = _t('上传目录无法写入, 请手动将安装目录下的 %s 目录的权限设置为可写然后继续升级', $uploadDir);
     }
 
