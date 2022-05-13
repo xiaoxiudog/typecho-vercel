c44fdc32aa03abf51e351d0c12db67c0f8a1f6e4
pbloods <lxtmltop@163.com>
2022-05-13 23:48:24 +0800

    typecho

--- config.inc.php
+++ config.inc.php
@@ -36,12 +36,12 @@ Typecho_Common::init();
 /** 定义数据库参数 */
 $db = new Typecho_Db('Pdo_Mysql', 'typecho_');
 $db->addServer(array (
-  'host' => 'sql.freedb.tech',
-  'user' => 'freedb_pblood',
-  'password' => 'Yhj#$Rz2xVWzc@2',
+  'host' => 'http://173.82.226.222:888/phpmyadmin_c876346039bc6377/index.php',
+  'user' => 'Pblood',
+  'password' => 'fkdzsnxfe2xTfJzS',
   'charset' => 'utf8mb4',
   'port' => '3306',
-  'database' => 'freedb_pblood',
+  'database' => 'pblood',
   'engine' => 'MyISAM',
 ), Typecho_Db::READ | Typecho_Db::WRITE);
 Typecho_Db::set($db);
